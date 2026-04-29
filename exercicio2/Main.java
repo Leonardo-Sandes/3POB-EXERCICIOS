298ecd885ada381761395ee22e60f0b5a3f4df07
Leonardo Sandes <leosandes11@gmail.com>
2026-04-29 17:24:28 -0300

    Add main class to display employee and manager data

---
+++ exercicio2/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        Funcionario funcionario = new Funcionario("João", 3000.0, 1);
+        Gerente gerente = new Gerente("Maria", 5000.0, 2);
+
+        System.out.println("Digite 1 para exibir os dados do funcionário ou 2 para exibir os dados do gerente:");
+        int escolha = leitor.nextInt();
+
+        if(escolha == 1) {
+            System.out.println("Dados do Funcionário:");
+            funcionario.exibirDados();
+        } else if(escolha == 2) {
+            System.out.println("Dados do Gerente:");
+            gerente.exibirDados();
+        } else {
+            System.out.println("Opção inválida.");
+        }
+        leitor.close();
+}
+}
