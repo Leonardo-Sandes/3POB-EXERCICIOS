8de0208277656cdb46400e37ab8b67b3c69d63b5
Leonardo Sandes <leosandes11@gmail.com>
2026-04-29 17:10:52 -0300

    Delete Moto.java

--- Moto.java
+++
@@ -1,9 +0,0 @@
-public class Moto extends Veiculo {
-    protected int cilindradas;
-
-    @Override
-    public void exibirDados() {
-        super.exibirDados(); 
-        System.out.println("A cilindrada da moto é: " + cilindradas);
-    }
-}
